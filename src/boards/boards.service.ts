a20ac6f3b458ca10a8303492f0b5c27eb04efadf
jaeha <jaeha23@naver.com>
2023-05-10 13:40:39 +0900

    create createBoard api

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+
+import { Board, BoardStatus } from './boards.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
