cb0e543a75f57928d10d28fec176fbd4a31301a1
jaeha <jaeha23@naver.com>
2023-05-10 17:22:26 +0900

    create user module

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 
 @Module({
